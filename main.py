7fd1123c3e0100153b7394ba83110c52a5eac661
gikyn713 <168406354+gikyn713@users.noreply.github.com>
2024-06-20 12:28:49 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from PIL import Image
+
+# 사이드 화면
+st.sidebar.header("로그인")
+user_id = st.sidebar.text_input('()')
+user_password = st.sidebar.text_input('()')
+
+if user_id = 'abc' and user_password == '1234':
+
+    st.sidebar.
